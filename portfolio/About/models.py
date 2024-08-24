1633cef7fbe623387816f1f7f554d7df09c0c494
Yadu <ykbs100@gmail.com>
2024-08-24 17:18:13 +0530

    Connected to PostgreSQL locally

--- portfolio/About/models.py
+++ portfolio/About/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 import uuid
-# Create your models here.
-
-
-#Creating Certifications table for about page
 
 class Certifications(models.Model):
-    cover_image_url = models.CharField(max_length=500, null=True, blank=True)
-    certification_name = models.CharField(max_length=700)
-    certification_description = models.TextField(max_length=1000)
-    certificate_image_url =  models.CharField(max_length=500, null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+        cover_image_url = models.CharField(max_length=500, null=True, blank=True)
+        certification_name = models.CharField(max_length=700)
+        certification_description = models.TextField(max_length=1000)
+        certificate_image_url =  models.CharField(max_length=500, null=True, blank=True)
 
-    def __str__(self) -> str:
-        return self.certification_name
+        created = models.DateTimeField(auto_now_add=True)
+        id =  models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+
+        def __str__(self) -> str:
+                return self.certification_name
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -90,10 +90,21 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD' : os.environ.get('PASSWORD'),
+        'HOST' : os.environ.get('HOST'),
+        'PORT' : os.environ.get('PORT'),
     }
 }
 
