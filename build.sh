9c51d46e4d32d2e01b1b1ce39607a71d57ca6920
Yadu Krishnan B <ykbs100@gmail.com>
2026-08-04 21:48:46 +0530

    Add automated superuser creation to build script

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations 
+python manage.py makemigrations
 python manage.py migrate
+
+# Create admin user automatically. '|| true' prevents the build from failing on future deploys when the user already exists.
+python manage.py createsuperuser --noinput --username admin --email ykbs100@gmail.com || true
